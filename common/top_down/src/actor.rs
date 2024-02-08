d2d22626a3c1224f6f58c3d9ba0c88490f5feb2f
porkbrain <git@porkbrain.com>
2024-02-08 19:00:22 +0100

    Draft of actor tiles

--- common/top_down/src/actor.rs
+++ common/top_down/src/actor.rs
@@ -3,7 +3,7 @@
 pub mod npc;
 pub mod player;
 
-use std::time::Duration;
+use std::{iter, time::Duration};
 
 use bevy::{
     ecs::event::event_update_condition,
@@ -11,13 +11,14 @@ use bevy::{
     time::Stopwatch,
     utils::{HashMap, HashSet},
 };
-use bevy_grid_squared::{GridDirection, Square};
+use bevy_grid_squared::{sq, GridDirection, Square};
 use bevy_inspector_egui::{prelude::ReflectInspectorOptions, InspectorOptions};
 use common_story::Character;
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    layout::{IntoMap, Tile},
+    layout::{IntoMap, Tile, TileIndex},
     Player, TileKind, TileMap,
 };
 
@@ -43,6 +44,14 @@ pub struct Actor {
     pub walking_to: Option<ActorTarget>,
     /// Used for animations.
     pub direction: GridDirection,
+    /// Squares that the actor currently occupies, along with the layer that
+    /// the actor tile kind is assigned to.
+    ///
+    /// The number of squares ultimately depend on a map layout, specifically
+    /// the size of a square. On more granular maps, the actor can occupy
+    /// more squares.
+    /// Also, different characters can occupy different squares.
+    occupies: Vec<TileIndex>,
 }
 
 /// Target for an actor to walk towards.
@@ -188,85 +197,114 @@ pub fn emit_movement_events<T: IntoMap>(
     }
 }
 
-/// Transform is used to change z index based on y.
+/// Actually moves the actors.
+/// Other systems will only edit the `Actor` component to plan the movement.
+///
+/// The z is based off y.
+/// See the [`IntoMap::extend_z`] for more info.
 pub fn animate_movement<T: IntoMap>(
     time: Res<Time>,
+    mut tilemap: ResMut<TileMap<T>>,
 
-    mut actors: Query<
-        (Entity, &mut Actor, &mut TextureAtlasSprite),
-        With<Transform>,
-    >,
-    // separate query so that we don't change transform unless needed
-    mut transform: Query<&mut Transform, With<Actor>>,
+    mut actors: Query<(
+        Entity,
+        &mut Actor,
+        &mut TextureAtlasSprite,
+        &mut Transform,
+    )>,
+) {
+    for (entity, mut actor, sprite, transform) in actors.iter_mut() {
+        if actor.occupies.is_empty() {
+            // One time initialization of the actor's tiles.
+            // It's more convenient to do it here than during
+            // the building process of the actor.
+            tilemap.replace_actor_tiles(entity, &mut actor);
+        }
+
+        animate_movement_for_actor::<T>(
+            &time,
+            &mut tilemap,
+            entity,
+            &mut actor,
+            sprite,
+            transform,
+        );
+    }
+}
+
+fn animate_movement_for_actor<T: IntoMap>(
+    time: &Time,
+    tilemap: &mut TileMap<T>,
+    entity: Entity,
+    actor: &mut Actor,
+    mut sprite: Mut<TextureAtlasSprite>,
+    mut transform: Mut<Transform>,
 ) {
     use GridDirection::*;
 
-    for (entity, mut actor, mut sprite) in actors.iter_mut() {
-        let current_direction = actor.direction;
-        let step_time = actor.step_time;
-        let standing_still_sprite_index = match current_direction {
-            Bottom => 0,
-            Top => 1,
-            Right | TopRight | BottomRight => 6,
-            Left | TopLeft | BottomLeft => 9,
+    let current_direction = actor.direction;
+    let step_time = actor.step_time;
+    let standing_still_sprite_index = match current_direction {
+        Bottom => 0,
+        Top => 1,
+        Right | TopRight | BottomRight => 6,
+        Left | TopLeft | BottomLeft => 9,
+    };
+
+    let Some(walking_to) = actor.walking_to.as_mut() else {
+        sprite.index = standing_still_sprite_index;
+
+        return;
+    };
+
+    walking_to.since.tick(time.delta());
+
+    let lerp_factor = walking_to.since.elapsed_secs()
+        / if let Top | Bottom | Left | Right = current_direction {
+            step_time.as_secs_f32()
+        } else {
+            // we need to walk a bit slower when walking diagonally because
+            // we cover more distance
+            step_time.as_secs_f32() * 2.0f32.sqrt()
         };
 
-        let Some(walking_to) = actor.walking_to.as_mut() else {
+    let to = T::layout().square_to_world_pos(walking_to.square);
+
+    if lerp_factor >= 1.0 {
+        let new_from = walking_to.square;
+
+        transform.translation = T::extend_z(to);
+
+        if let Some((new_square, new_direction)) = walking_to.planned.take() {
+            walking_to.since.reset();
+            walking_to.square = new_square;
+            actor.direction = new_direction;
+        } else {
             sprite.index = standing_still_sprite_index;
 
-            continue;
+            actor.walking_to = None;
+        }
+
+        actor.walking_from = new_from;
+
+        tilemap.replace_actor_tiles(entity, actor);
+    } else {
+        let animation_step_time =
+            animation_step_secs(step_time.as_secs_f32(), current_direction);
+        let extra = (time.elapsed_seconds_wrapped() / animation_step_time)
+            .floor() as usize
+            % 2;
+
+        sprite.index = match current_direction {
+            Top => 2 + extra,
+            Bottom => 4 + extra,
+            Right | TopRight | BottomRight => 7 + extra,
+            Left | TopLeft | BottomLeft => 10 + extra,
         };
 
-        walking_to.since.tick(time.delta());
+        let from = T::layout().square_to_world_pos(actor.walking_from);
 
-        let lerp_factor = walking_to.since.elapsed_secs()
-            / if let Top | Bottom | Left | Right = current_direction {
-                step_time.as_secs_f32()
-            } else {
-                // we need to walk a bit slower when walking diagonally because
-                // we cover more distance
-                step_time.as_secs_f32() * 2.0f32.sqrt()
-            };
-
-        // safe cuz <With<Transform>>
-        let mut transform = transform.get_mut(entity).unwrap();
-        let to = T::layout().square_to_world_pos(walking_to.square);
-
-        if lerp_factor >= 1.0 {
-            let new_from = walking_to.square;
-
-            transform.translation = T::extend_z(to);
-
-            if let Some((new_square, new_direction)) = walking_to.planned.take()
-            {
-                walking_to.since.reset();
-                walking_to.square = new_square;
-                actor.direction = new_direction;
-            } else {
-                sprite.index = standing_still_sprite_index;
-
-                actor.walking_to = None;
-            }
-
-            actor.walking_from = new_from;
-        } else {
-            let animation_step_time =
-                animation_step_secs(step_time.as_secs_f32(), current_direction);
-            let extra = (time.elapsed_seconds() / animation_step_time).floor()
-                as usize
-                % 2;
-
-            sprite.index = match current_direction {
-                Top => 2 + extra,
-                Bottom => 4 + extra,
-                Right | TopRight | BottomRight => 7 + extra,
-                Left | TopLeft | BottomLeft => 10 + extra,
-            };
-
-            let from = T::layout().square_to_world_pos(actor.walking_from);
-
-            transform.translation = T::extend_z(from.lerp(to, lerp_factor));
-        }
+        transform.translation = T::extend_z(from.lerp(to, lerp_factor));
     }
 }
 
@@ -394,6 +432,11 @@ impl CharacterBundleBuilder {
     /// - [`Name`] component with the character's name
     /// - [`Actor`] component with the character's movement data
     /// - [`SpriteSheetBundle`] with the character's sprite atlas
+    ///
+    /// # Important
+    /// Since we don't yet have entity, we don't insert tiles into the
+    /// tilemap. This will be immediately remedied in the
+    /// [`animate_movement`] system.
     pub fn build<T: IntoMap>(self) -> impl Bundle {
         let CharacterBundleBuilder {
             character,
@@ -406,6 +449,9 @@ impl CharacterBundleBuilder {
 
         let step_time = step_time.unwrap_or(character.default_step_time());
 
+        // see the method docs
+        let occupies = default();
+
         (
             Name::from(character.name()),
             Actor {
@@ -414,6 +460,7 @@ impl CharacterBundleBuilder {
                 direction: initial_direction,
                 walking_from: T::layout().world_pos_to_square(initial_position),
                 walking_to,
+                occupies,
             },
             SpriteSheetBundle {
                 texture_atlas: character.sprite_atlas_handle(),
@@ -429,5 +476,91 @@ impl CharacterBundleBuilder {
                 ..default()
             },
         )
+    }
+}
+
+const O: Square = sq(0, 0);
+
+lazy_static! {
+    /// I tried implementing Bresenham's circle (filled) and couple of
+    /// other options.
+    /// Nothing felt satisfactory in shape.
+    /// Given that most of the time we will have standard radii and layouts,
+    /// and that this logic runs on every actor movement, implementing a
+    /// static felt right.
+    /// The squares are centered around (0, 0) and the actor's current
+    /// position needs to be added to each square, or equivalently the center
+    /// must be subtracted from each square before checking `contains`.
+    ///
+    /// The second tuple member is the distance from origin in squares.
+    static ref ACTOR_ZONE_AT_ORIGIN: Vec<(Square, usize)> = {
+        let distance_two = vec![
+                        sq(-1, 2),sq(0, 2),sq(1, 2),
+                sq(-2,1),                            sq(2,1),
+                sq(-2,0),          /*O*/             sq(2,0),
+                sq(-2,-1),                           sq(2,-1),
+                        sq(-1,-2),sq(0,-2),sq(1,-2),
+        ];
+        let distance_three = vec![
+        sq(-3,0),                                             sq(3,0),
+        ];
+
+        iter::once((O, 0))
+            .chain(O.neighbors().map(|sq| (sq, 1)))
+            .chain(distance_two.into_iter().map(|sq| (sq, 2)))
+            .chain(distance_three.into_iter().map(|sq| (sq, 3)))
+            .collect()
+    };
+}
+
+impl<T: IntoMap> TileMap<T> {
+    /// The direction the actor is facing offsets the center (`actor_stands_on`)
+    /// of the circle that will be converted to occupied squares.
+    fn replace_actor_tiles(&mut self, entity: Entity, actor: &mut Actor) {
+        // Round up the radius to the nearest tile
+        let tile_radius = (actor.character.actor_personal_zone_px()
+            / T::layout().square_size)
+            .ceil() as i32;
+
+        let mut actor_zone_at_origin = ACTOR_ZONE_AT_ORIGIN.clone();
+
+        let center = actor
+            .walking_to
+            .as_ref()
+            .map(|t| t.square)
+            .unwrap_or(actor.walking_from)
+            .neighbor(GridDirection::Top); // looks better when pushed a bit forward
+
+        actor.occupies.retain(|(sq, layer)| {
+            let sq_origin = sq - center; // center around origin
+
+            // keep overlapping squares
+            if actor_zone_at_origin.remove(&sq_origin) {
+                return true;
+            }
+
+            // remove the non-overlapping squares
+            // we no longer use origin because we want to remove the tile from
+            // the tilemap with its actual position
+            let prev_tile =
+                self.set_tile_kind_layer(*sq, *layer, TileKind::Empty);
+            debug_assert_eq!(TileKind::Actor(entity), prev_tile);
+
+            false
+        });
+        // then for the remaining squares that don't have the actor yet
+        for sq_origin in actor_zone_at_origin {
+            let sq = sq_origin + center; // center around actor
+
+            // if we didn't do this then it'd be possible for actor A to occupy
+            // all tiles around actor B, preventing their movement
+            if !self.is_walkable(sq, entity) {
+                continue;
+            }
+
+            let layer =
+                self.add_tile_to_first_empty_layer(sq, TileKind::Actor(entity));
+            actor.occupies.push((sq, layer));
+        }
     }
 }
