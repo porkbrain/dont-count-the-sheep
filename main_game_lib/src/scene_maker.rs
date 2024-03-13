622296fbb7a4137f30e07823eca2eeb3ff96c55a
porkbrain <git@porkbrain.com>
2024-03-13 23:12:08 +0100

    Deleting previous implementation which is no longer needed as we use godot now

--- main_game_lib/src/scene_maker.rs
+++ main_game_lib/src/scene_maker.rs
@@ -10,12 +10,8 @@
 //! system.
 
 pub(crate) mod store_and_load;
-#[cfg(feature = "devtools")]
-mod toolbar;
 
 use bevy::ecs::{entity::Entity, query::With, system::Query};
-#[cfg(feature = "devtools")]
-pub use plugin::Plugin;
 pub use store_and_load::{
     LoadedFromSceneFile, SceneSerde, SceneSpriteAtlas, SceneSpriteConfig,
     SpriteScene, SpriteSceneHandle,
@@ -28,65 +24,4 @@ pub fn are_sprites_spawned_and_file_despawned<T: SpriteScene>() -> impl FnMut(
 )
     -> bool {
     move |entities, handle| !entities.is_empty() && handle.is_empty()
-}
-
-#[cfg(feature = "devtools")]
-mod plugin {
-    use bevy::{
-        app::{App, Update},
-        ecs::schedule::{
-            common_conditions::in_state, IntoSystemConfigs, OnEnter, OnExit,
-            States,
-        },
-    };
-    use common_action::interaction_just_pressed;
-
-    use super::*;
-    use crate::StateSemantics;
-
-    /// A plugin that allows in-game scene creation and editing.
-    pub struct Plugin<T, S> {
-        states: StateSemantics<S>,
-        _phantom: std::marker::PhantomData<T>,
-    }
-
-    impl<T, S> Plugin<T, S> {
-        /// Create a new scene maker plugin.
-        pub fn new(states: StateSemantics<S>) -> Self {
-            Self {
-                states,
-                _phantom: std::marker::PhantomData,
-            }
-        }
-    }
-
-    impl<T: SpriteScene, S: States + Copy> bevy::app::Plugin for Plugin<T, S> {
-        fn build(&self, app: &mut App) {
-            use bevy_inspector_egui::quick::ResourceInspectorPlugin;
-
-            use self::toolbar::SceneMakerToolbar;
-
-            app.register_type::<SceneMakerToolbar>().add_plugins(
-                ResourceInspectorPlugin::<SceneMakerToolbar>::default(),
-            );
-
-            app.add_systems(
-                Update,
-                store_and_load::store
-                    .run_if(in_state(self.states.running))
-                    .run_if(interaction_just_pressed()),
-            );
-
-            app.add_systems(OnEnter(self.states.loading), toolbar::spawn)
-                .add_systems(
-                    Update,
-                    (
-                        toolbar::move_sprite_system,
-                        store_and_load::react_to_changes::<T>,
-                    )
-                        .run_if(in_state(self.states.running)),
-                )
-                .add_systems(OnExit(self.states.quitting), toolbar::despawn);
-        }
-    }
 }
