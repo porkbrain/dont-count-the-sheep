b6566490b35f7fd497f3870b489652119e5b02c9
shaperka <161528618+shaperka@users.noreply.github.com>
2024-05-30 11:53:04 +0200

    clinic move

--- scenes/downtown/src/autogen/zone_tile_impl.rs
+++ scenes/downtown/src/autogen/zone_tile_impl.rs
@@ -19,6 +19,7 @@ Self::TwinpeaksApartmentEntrance => Some(ZoneGroup(5)),
     { Self::Building1Entrance => Some(33),
 Self::SewersEntrance => Some(60),
 Self::MallEntrance => Some(145),
+Self::ClinicEntrance => Some(6),
 Self::PlantShopEntrance => Some(44),
 Self::TwinpeaksApartmentEntrance => Some(44),
  #[allow(unreachable_patterns)] _ => None, }
